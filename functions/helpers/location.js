6ce794c7a10bfbc5fec0df4748a4b35907c11a07
Kirill Kliavin <k.kliavin@gmail.com>
2024-04-24 19:16:40 +1000

    default location

--- functions/helpers/location.js
+++ functions/helpers/location.js
@@ -4,12 +4,12 @@ export default function getLocation(cf) {
   //console.log(cf)
 
   const result = {
-    city: cf.city,
-    region: cf.region,
-    country_code: cf.country,
-    timezone: cf.timezone,
-    latitude: cf.latitude,
-    longitude: cf.longitude
+    city: cf.city || 'Los Angeles',
+    region: cf.region || 'California',
+    country_code: cf.country || 'US',
+    timezone: cf.timezone || 'America/Los_Angeles',
+    latitude: cf.latitude || 34.0522,
+    longitude: cf.longitude || -118.2437
   }
 
   result.country = getCountryName(result.country_code)
